1e6ccf1583bc4797d155dc47b9ca1fed0eaccfde
Leticia Vieira <lvieira268@gmail.com>
2021-04-06 16:04:14 -0300

    conclui desafio
    Signed-off-by: Leticia Vieira <lvieira268@gmail.com>

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import React from 'react';
+
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+interface ISidebarProps {
+  genres: GenreResponseProps;
+  selectedGenreId: number;
+  handleClickButton: (genre: Omit<GenreResponseProps, 'id'>) => void;
+}
+
+
+const Sidebar: React.FC<ISidebarProps> = ({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}) => {
+  return (
+    <Button
+      key={String(genres.id)}
+      title={genres.title}
+      iconName={genres.name}
+      selected={selectedGenreId === genres.id}
+      onClick={() => handleClickButton(genres)}
+    />
+  )
+}
+
+export default Sidebar;
